72980d5408674eb4eeff0fc27fd1320329a4b668
Thomas Hamshere-Clarke <thomas.hamshere.clarke@gmail.com>
2022-02-09 10:03:44 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 copy zshrc.template ~/.zshrc
 cd ~ && mkdir workspace && cd !*
+
+# homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.zshrc
+
+# git & github
 brew install git bash-completion
 brew install gh
-
 git config --global user.email "thomas.hamshere.clarke@gmail.com"
 git config --global user.name "Thomas Hamshere-Clarke"
+./git_alias.sh
 
+# nvm & node
+brew install nvm
 nvm install --lts
 npm install -g npm yarn
 
+# apps
 brew install --cask chrome
 brew install --cask firefox
 brew install --cask dropbox
@@ -21,3 +27,4 @@ brew install --cask docker
 brew install --cask cleanshot
 brew install --cask microsoft-teams
 brew install --cask microsoft-outlook
+
